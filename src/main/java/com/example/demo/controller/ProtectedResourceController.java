e52730e2741ce70fad9c12b29d4f8f464568e9cd
SpellZZZ <james321321@wp.pl>
2024-03-13 19:42:16 +0100

    ADD spring security with jwt

---
+++ src/main/java/com/example/demo/controller/ProtectedResourceController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProtectedResourceController {
+
+    @GetMapping("/protected")
+    @Secured("ROLE_USER") // Role-based authorization
+    public String protectedResource() {
+        return "This is a protected resource.";
+    }
+}
