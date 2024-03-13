ed4038275c2cc27e50d3802f7c3f1b9cb08f7c99
SpellZZZ <james321321@wp.pl>
2024-03-13 03:15:00 +0100

    Init commit

---
+++ src/test/java/com/example/demo/MainControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainControllerTest {
+
+    @Test
+    void allQuestions() {
+    }
+
+    @Test
+    void addQuestion() {
+    }
+
+    @Test
+    void singleQuestion() {
+    }
+
+    @Test
+    void allTopics() {
+    }
+
+    @Test
+    void addTopics() {
+    }
+}
