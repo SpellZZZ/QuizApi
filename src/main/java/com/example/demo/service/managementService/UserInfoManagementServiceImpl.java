3636b2fe20d77abd2b6b96c30b5227bde7bd8422
SpellZZZ <james321321@wp.pl>
2024-03-14 23:23:01 +0100

    Fix refactor 2

--- src/main/java/com/example/demo/service/managementService/UserInfoManagementServiceImpl.java
+++ src/main/java/com/example/demo/service/managementService/UserInfoManagementServiceImpl.java
@@ -9,6 +9,8 @@ import com.example.demo.model.UserInfo;
 import com.example.demo.service.dbService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -32,11 +35,13 @@ public class UserInfoManagementServiceImpl implements UserInfoManagementService,
 
         UserInfo userDetail = userService.getUserByName(username);
 
-
+        ArrayList<GrantedAuthority> roles = new ArrayList<>();
+        GrantedAuthority ga = new SimpleGrantedAuthority(userDetail.getRoles());
+        roles.add(ga);
 
         return new org.springframework.security.core.userdetails.User(userDetail.getName(),
                 userDetail.getPassword(),
-                new ArrayList<>()
+                roles
 
         );
 
